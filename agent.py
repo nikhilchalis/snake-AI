a60def09e0064a6c732646a513aad79933797e2a
nikhilchalis <nikhilchalis@gmail.com>
2023-09-19 22:26:56 +1000

    initialising agent class

--- agent.py
+++ agent.py
@@ -2,6 +2,75 @@ import torch
 import random
 import numpy as np
 from collections import deque
-from game import SnakeGameAI, Direction, Point
+from game import SnakeGameAI, Direction, Point, BLOCK_SIZE
+
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001
+
+class Agent:
+    def __init__(self):
+        self.n_games = 0
+        self.epsilon = 0 # controls randomness
+        self.gamma = 0 # discount rate
+        self.memory = deque(maxlen=MAX_MEMORY) # if exceeded, automatically removes elements from the left for us
+        pass
+
+    def get_state(self, game):
+        pass
+
+    def remember(self, state, action, reward, next_state, done):
+        pass
+
+    def train_long_memory(self):
+        pass
+
+    def train_short_memory(self):
+        pass
+
+    def get_action(self, state):
+        pass
+
+def train():
+    plot_scores = []
+    plot_mean_scores = []
+    total_score = 0
+    record = 0
+    agent = Agent()
+    game = SnakeGameAI()
+    while True:
+        # get old state
+        state_old = agent.get_state(game)
+
+        # get move
+        final_move = agent.get_action(state_old)
+
+        # perform move and get new state
+        reward, done, score = game.play_step(final_move)
+        state_new = agent.get_state(game)
+
+        # train short memory
+        agent.train_short_memory(state_old, final_move, reward, state_new, done)
+
+        # remember
+        agent.remember(state_old, final_move, reward, state_new, done)
+
+        if done:
+            # train long memory - trains on all the games before
+            game.reset()
+            agent.n_games += 1
+            agent.train_long_memory()
+
+            if score > record:
+                record = score
+                # TODO: agent.model.save() f0r later
+
+            print('Game ', agent.n_games, 'Score ', score, 'Record ', record)
+
+            # TODO: Plotting
 
 
+
+
+if __name__ == '__main__':
+    train()
